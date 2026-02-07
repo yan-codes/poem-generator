1dccd26a76b3c61654904df5d9d9d248d1b7b547
Dianne Louise Cardenas <dlpcardenas@gmail.com>
2026-02-07 17:41:30 +0800

    Added basic form handling and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings:
+      "A university degree, four books, and hundreds of articles and I still make mistakes when reading, You write to me 'good morning' and I read it as, 'I love you'.",
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
