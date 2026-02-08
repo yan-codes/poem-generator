ce52fec3db5b2097e5fad092e5207822ff360be5
Dianne Louise Cardenas <dlpcardenas@gmail.com>
2026-02-08 22:41:09 +0800

    Improved user experience, added css loader and removed console.logs

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "74b8442a4oa25823t288ab70d171ef48";
   let context =
-    "You are a well-versed short poem expert. Your task is to generate a 4 line poem. Make sure to follow user instructions and separate each line with a <br />. Sign the poem with 'SheCodes AI🌸' inside a <em> element";
+    "You are a witty poem expert. Your task is to generate a 4 line poem. Make sure to follow user instructions and separate each line with a <br />. Sign the poem with 'SheCodes AI🌸' inside a <em> element";
   let prompt = `User instructions: Generate a short poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="loader"></div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
